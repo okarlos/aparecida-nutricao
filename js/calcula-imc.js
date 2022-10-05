5b0a3ee528a0f927b573827438c5b23c36eb8cf7
Karlos Kliemann <karloskliemann@gmail.com>
2022-10-05 07:56:52 -0300

    update js, creating functions

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -32,48 +32,13 @@ for (let i = 0 ; i < listPacientes.length ; i++) {
     }
 
     if (pesoValido && alturaValida) {
-        let imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+        let imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
     }
-
 }
 
-let botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event) {
-    event.preventDefault();
-
-    let form = document.querySelector("#form-adiciona");
-
-    let nome = form.nome.value;
-    let peso = form.peso.value;
-    let altura = form.altura.value;
-    let gordura = form.gordura.value;
-
-    let pacienteTr = document.createElement("tr");
-
-    let nomeTd = document.createElement("td");
-    let pesoTd = document.createElement("td");
-    let alturaTd = document.createElement("td");
-    let gorduraTd = document.createElement("td");
-    let imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    let tabela = document.querySelector("#tabela-pacientes");
-    
-    tabela.appendChild(pacienteTr)
-
-    nome = null;
-    peso = '';
-    altura = '';
-    gordura = '';
-
-})
+function calculaImc(peso, altura) {
+    let imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
--- js/form.js
+++ js/form.js
@@ -3,37 +3,63 @@ botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
 
     let form = document.querySelector("#form-adiciona");
+    
+    // extraindo informações do form
+    let paciente = obtemInfoForm(form);
 
-    let nome = form.nome.value;
-    let peso = form.peso.value;
-    let altura = form.altura.value;
-    let gordura = form.gordura.value;
+    // cria os elementos
+    let pacienteTr = montaTr(paciente);
 
+    // adiciona o paciente na tabela
+    let tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr)
+})
+
+function obtemInfoForm(form) {
+
+    let paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+
+}
+
+function montaTr(paciente) {
+    
+    // cria uma linha
     let pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
 
+    // cria as colunas
     let nomeTd = document.createElement("td");
+    nomeTd.classList.add("info-nome");
     let pesoTd = document.createElement("td");
+    pesoTd.classList.add("info-peso");
     let alturaTd = document.createElement("td");
+    alturaTd.classList.add("info-altura");
     let gorduraTd = document.createElement("td");
+    gorduraTd.classList.add("info-gordura");
     let imcTd = document.createElement("td");
+    imcTd.classList.add("info-imc");
 
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
+    // preenche as colunas com os dados do paciente
+    nomeTd.textContent = paciente.nome;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent = paciente.imc;
 
+    // inclui as colunas na linha
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
 
-    let tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr)
-
-    nome = null;
-    peso = '';
-    altura = '';
-    gordura = '';
-
-})
+    return pacienteTr;
+}
