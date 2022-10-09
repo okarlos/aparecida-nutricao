b33de8eee8977efd5d490640ebbb647ce7b9d09a
Karlos Kliemann <karloskliemann@gmail.com>
2022-10-09 07:47:16 -0300

    add remove effect

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,5 +1,10 @@
 var tabela = document.getElementById("tabela");
 
 tabela.addEventListener("dblclick", function (event) {
-    event.target.parentNode.remove();
+
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function() {
+        event.target.parentNode.remove();
+    }, 500)
 })
